ac045b9c4f3c5da1767dd792ab0d924ac95cc5c3
Savita Chauhan <schauhan@divisionsinc.com>
2022-03-13 18:40:09 -0400

    Exercise 05 Styles (with extra credit)

--- src/exercise/05.js
+++ src/exercise/05.js
@@ -14,16 +14,62 @@ import '../box-styles.css'
 // 🐨 also use the style prop to make the font italic
 // 💰 Here are available style attributes: backgroundColor, fontStyle
 
-const smallBox = <div>small lightblue box</div>
-const mediumBox = <div>medium pink box</div>
-const largeBox = <div>large orange box</div>
+// original code
+// const smallBox = <div>small lightblue box</div>
+// const mediumBox = <div>medium pink box</div>
+// const largeBox = <div>large orange box</div>
 
+// 1...
+// const smallBox = <div className="box box--small" style={{backgroundColor: "lightblue", fontStyle: "italic"}}>small lightblue box</div>
+// const mediumBox = <div className="box box--medium" style={{backgroundColor: "pink", fontStyle: "italic"}}>medium pink box</div>
+// const largeBox = <div className="box box--large" style={{backgroundColor: "orange", fontStyle: "italic"}}>large orange box</div>
+
+// function App() {
+//   return (
+//     <div>
+//       {smallBox}
+//       {mediumBox}
+//       {largeBox}
+//     </div>
+//   )
+// }
+
+// // 2... Extra credit - 1 Make custom component
+// function Box(props) {
+//   const eleClassName = `box ${props.className}`
+//   const eleStyle = {fontStyle: "italic", ...props.style}
+//   const ele = <div className={eleClassName} style={eleStyle}>{props.children}</div>
+//   return ele
+// }
+// function App() {
+//   return (
+//     <div>
+//       <Box className="box--small" style={{backgroundColor: 'lightblue'}}>
+//         small lightblue box
+//       </Box>
+//     </div>
+//   )
+// }
+
+// 3... Extra credit - 2 
+function Box(props) {
+  const eleClassName = `box box--${props.size}`
+  const eleStyle = {fontStyle: "italic", ...props.style}
+  const ele = <div className={eleClassName} style={eleStyle}>{props.children}</div>
+  return ele
+}
 function App() {
   return (
     <div>
-      {smallBox}
-      {mediumBox}
-      {largeBox}
+      <Box size="small" style={{backgroundColor: 'lightblue'}}>
+        small lightblue box
+      </Box>
+      <Box size="medium" style={{backgroundColor: 'pink'}}>
+        medium pink box
+      </Box>
+      <Box size="large" style={{backgroundColor: 'orange'}}>
+        large orange box
+      </Box>
     </div>
   )
 }
